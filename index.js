d169a25981123e5f909ab89fe41355ebf7a77241
tbrew1023 <tbrew1023@gmail.com>
2022-02-18 11:31:14 -0600

    genesis

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello from hermes');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name ? ', (name) => {
+  rl.question('Where do you live ? ', (country) => {
+    console.log(`${name}, is a citizen of ${country}`);
+    rl.close();
+  });
+});
+
+rl.on('close', function () {
+  console.log('\nSee ya');
+  process.exit(0);
+});
+
+
